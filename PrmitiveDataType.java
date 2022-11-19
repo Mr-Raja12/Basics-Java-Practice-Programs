caae37b1fad981c3ffb915abe028d9eab08da93c
Mr-Raja12 <rk9576899082@gmail.com>
2022-11-20 00:15:50 +0530

    Added Defult value of all Primitive Data Type

---
+++ PrmitiveDataType.java
@@ -0,0 +1,22 @@
+package com.bl.practiceProblems;
+public class PrmitiveDataType {
+	static int i;
+	static byte b;
+	static short s;
+	static long l;
+	static double d;    //initialized the all Data types a static so there is no need of object for the same
+	static float f;
+	static char ch;
+	static boolean b1;
+	public static void main(String[] args) {
+		System.out.println("Defult Values Of All Primitive Data Type Is : ");
+		System.out.println("Int " +i);
+		System.out.println("short "+s);
+		System.out.println("long "+l);
+		System.out.println("double "+d);
+		System.out.println("float "+f);
+		System.out.println("char "+ch);
+		System.out.println("boolean "+b1);
+
+	}
+}
