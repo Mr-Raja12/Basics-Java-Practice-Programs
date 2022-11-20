6201501e95221b4b4d7e296bf78aff5bfa9cb4c2
Mr-Raja12 <rk9576899082@gmail.com>
2022-11-20 10:21:41 +0530

    Added a Program to find the sum of command-line arguments

---
+++ CommandLineArgument.java
@@ -0,0 +1,14 @@
+package com.bl.practiceProblems;
+
+public class CommandLineArgument {
+       public static void main(String[] args) {
+    	  int i, sum=0;
+    	  for(i=0;i<=args.length;i++)
+    	  {
+    		  sum=sum+Integer.parseInt(args[i]);
+    
+    	  }
+    	  System.out.println("Sum "+sum);
+       }
+       
+}
